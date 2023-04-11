8e3467a7c306730f2e90fea83c9640d309f3b577
alex999990009 <alex999990009@gmail.com>
2023-04-11 15:10:49 +0200

    SquashedData QuickFix

--- src/main/kotlin/org/arend/highlight/BasePass.kt
+++ src/main/kotlin/org/arend/highlight/BasePass.kt
@@ -335,18 +335,7 @@ abstract class BasePass(open protected val file: IArendFile, editor: Editor, nam
                     }
                 PATTERN_IGNORED -> if (cause is Abstract.Pattern) registerFix(info, ReplaceWithWildcardPatternQuickFix(SmartPointerManager.createPointer(cause)))
                 COULD_BE_LEMMA, AXIOM_WITH_BODY -> if (cause is ArendDefFunction) registerFix(info, ReplaceFunctionKindQuickFix(SmartPointerManager.createPointer(cause.functionKw), FunctionKind.LEMMA))
-                LEVEL_IGNORED -> {
-                    var element: PsiElement? = cause
-                    while (element !is ArendReturnExpr) {
-                        element = element?.parent
-                        if (element == null) {
-                            break
-                        }
-                    }
-                    if (element is ArendReturnExpr) {
-                        registerFix(info, RemoveLevelQuickFix(SmartPointerManager.createPointer(cause)))
-                    }
-                }
+                LEVEL_IGNORED -> registerFix(info, RemoveLevelQuickFix(SmartPointerManager.createPointer(cause)))
                 else -> {}
             }
 
@@ -404,6 +393,10 @@ abstract class BasePass(open protected val file: IArendFile, editor: Editor, nam
 
             is ElimSubstError -> {
                 registerFix(info, ElimSubstQuickFix(SmartPointerManager.createPointer(cause), error))
+            }
+
+            is SquashedDataError -> {
+                registerFix(info, SquashedDataQuickFix(SmartPointerManager.createPointer(cause)))
             }
 
             is TypecheckingError -> for (quickFix in error.quickFixes) {
